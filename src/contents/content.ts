da9eb7db73a924d4722eb96e72e62bf848f4ce17
obutora <obutora@gmail.com>
2024-03-20 22:16:28 +0900

    remove useless code

--- src/contents/content.ts
+++ src/contents/content.ts
@@ -1,13 +1,3 @@
-// const body = document.querySelector("body");
-// if (body) {
-//   const button = document.createElement("button");
-//   button.innerText = "アラートを表示";
-//   button.addEventListener("click", () => {
-//     alert("おめでとうございます！");
-//   });
-//   body.append(button);
-// }
-
 chrome.runtime.onMessage.addListener((request) => {
   console.log(`sendReq: ${request}`)
   let selectedDom;
@@ -17,10 +7,6 @@ chrome.runtime.onMessage.addListener((request) => {
       console.log(request.message)
       selectedDom = window.getSelection()?.anchorNode?.parentNode;
       selectedDom!.textContent = request.message;
-
-      // if (!selectedDom) { 
-      //     selectedDom!.innerHTML = "<h1>Hello !</h1>";
-      // }
 
       break;
     case 'text':
